2e083d894af9ec9459bfa9c7a081f2633462fcb1
rgm <git@rushd.xyz>
2025-12-02 10:01:01 +0000

    0.2.0 Created Customer subclass

---
+++ src/main/java/ie/atu/Customer.java
@@ -0,0 +1,41 @@
+package ie.atu;
+
+public class Customer extends Person
+{
+    private int customerNumber;
+    private boolean mailingList;
+
+    public Customer() {
+        super();
+        this.customerNumber = 0;
+        this.mailingList = false;
+    }
+
+    public Customer(String name, String address, String phoneNumber, int customerNumber, boolean mailingList) {
+        super(name, address, phoneNumber);
+        this.customerNumber = customerNumber;
+        this.mailingList = mailingList;
+    }
+
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public boolean isMailingList() {
+        return mailingList;
+    }
+
+    public void setMailingList(boolean mailingList) {
+        this.mailingList = mailingList;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " customerNumber=" + customerNumber +
+                ", mailingList=" + mailingList;
+    }
+}
