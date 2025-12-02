4fa74b61faeda598e3b5c8b611929f9998ce8f1f
rgm <git@rushd.xyz>
2025-12-02 11:27:19 +0000

    0.5.0 Constructors for PreferredCustomer

--- src/main/java/ie/atu/PreferredCustomer.java
+++ src/main/java/ie/atu/PreferredCustomer.java
@@ -5,6 +5,25 @@ public class PreferredCustomer extends Customer
     private int loyaltyPoints;
     private double discountLevel;
 
+    public PreferredCustomer()
+    {
+        super();
+        this.loyaltyPoints = 0;
+        this.discountLevel = 0.0;
+    }
+
+    public PreferredCustomer(String name, String address, String phoneNumber, int customerNumber, boolean mailingList,
+                             int loyaltyPoints)
+    {
+        super(name, address,phoneNumber,customerNumber,mailingList);
+        this.loyaltyPoints = loyaltyPoints;
+        updateDiscountLevel(loyaltyPoints);
+    }
+
+    private void updateDiscountLevel(int loyaltyPoints)
+    {
+    }
+
     public int getLoyaltyPoints()
     {
         return loyaltyPoints;
