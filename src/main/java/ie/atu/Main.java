61ab8c187742e266fc870ca4f2ee76293e5a9fe3
Rushdius Gluteus Maximus <git@rushd.xyz>
2025-12-02 09:33:45 +0000

    Person superclass (#1)
    0.1.0 Created person super class
    
    0.1.1 Test Person class in main

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -3,5 +3,11 @@ package ie.atu;
 public class Main {
     public static void main(String[] args)
     {
+        Person firstPerson = new Person();
+        firstPerson.setName("Jeff");
+        firstPerson.setAddress("Jeff's Island");
+        firstPerson.setPhoneNumber("123456");
+
+        System.out.println(firstPerson);
     }
 }
--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -2,4 +2,50 @@ package ie.atu;
 
 public class Person
 {
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public Person() {
+        this.name = "";
+        this.address = "";
+        this.phoneNumber = "";
+    }
+
+    public Person(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'';
+    }
 }
